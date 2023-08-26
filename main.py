8e2e999d7fcd8ddd90e5e304c9b9dffd0d5f8366
maxl85 <maxl85@mail.ru>
2023-08-26 12:08:40 +0300

    mask_cam

--- main.py
+++ main.py
@@ -27,6 +27,22 @@ API_FILES_UPLOAD = os.environ["API_FILES_UPLOAD"]
 # Initialize the models
 yolo_model = YOLO(os.environ["YOLO_MODEL"])
 
+mask_cam1_1_yolo = cv2.imread("./mask/mask_cam1_1_yolo.jpg")
+mask_cam1_1_yolo = cv2.cvtColor(mask_cam1_1_yolo, cv2.COLOR_BGR2GRAY)
+mask_cam1_1_yolo = cv2.threshold(mask_cam1_1_yolo, 200, 255, cv2.THRESH_BINARY)[1]
+
+mask_cam1_2_yolo = cv2.imread("./mask/mask_cam1_2_yolo.jpg")
+mask_cam1_2_yolo = cv2.cvtColor(mask_cam1_2_yolo, cv2.COLOR_BGR2GRAY)
+mask_cam1_2_yolo = cv2.threshold(mask_cam1_2_yolo, 200, 255, cv2.THRESH_BINARY)[1]
+
+mask_cam2_1_yolo = cv2.imread("./mask/mask_cam2_1_yolo.jpg")
+mask_cam2_1_yolo = cv2.cvtColor(mask_cam2_1_yolo, cv2.COLOR_BGR2GRAY)
+mask_cam2_1_yolo = cv2.threshold(mask_cam2_1_yolo, 200, 255, cv2.THRESH_BINARY)[1]
+
+mask_cam2_2_yolo = cv2.imread("./mask/mask_cam2_2_yolo.jpg")
+mask_cam2_2_yolo = cv2.cvtColor(mask_cam2_2_yolo, cv2.COLOR_BGR2GRAY)
+mask_cam2_2_yolo = cv2.threshold(mask_cam2_2_yolo, 200, 255, cv2.THRESH_BINARY)[1]
+
 ###################### FastAPI Setup #############################
 
 # title
@@ -76,14 +92,32 @@ async def redirect():
 
 ######################### MAIN Func #################################
 
+def plot_rectangle(img, res):
+    img1 = img
+    (x1, y1) = (int(res[0].boxes.xyxy[0][0]), int(res[0].boxes.xyxy[0][1]))
+    (x2, y2) = (int(res[0].boxes.xyxy[0][2]), int(res[0].boxes.xyxy[0][3]))
+    cv2.rectangle(img1, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    return img1
+
 @app.post("/object_detection", status_code=status.HTTP_201_CREATED)
 def object_detection(file: UploadFile, camId: str = Form(...), dateTime: str = Form(...)):
     
+    img1 = Image.open(io.BytesIO(file.file.read())).convert("RGB")
     
-    input_image = Image.open(io.BytesIO(file.file.read())).convert("RGB")
-    
-    res = yolo_model.predict(input_image, conf=float(os.environ["YOLO_CONF"]), classes=0)
-    plot_res = res[0].plot(labels=False)
+    if (camId == 'cam1_1'):
+        img1m = cv2.bitwise_and(img1, img1, mask = mask_cam1_1_yolo)
+    if (camId == 'cam1_2'):
+        img1m = cv2.bitwise_and(img1, img1, mask = mask_cam1_2_yolo)
+    if (camId == 'cam2_1'):
+        img1m = cv2.bitwise_and(img1, img1, mask = mask_cam2_1_yolo)
+    if (camId == 'cam2_2'):
+        img1m = cv2.bitwise_and(img1, img1, mask = mask_cam2_2_yolo)
+        
+    res = yolo_model.predict(img1m, conf=float(os.environ["YOLO_CONF"]), classes=0)
+    if (len(res[0].boxes.xyxy) > 0):
+        plot_res = plot_rectangle(img1, res)
+    else:
+        plot_res = img1
     
     if (len(res[0].boxes.cls) > 0):
         predicted_img = directory_found + file.filename
